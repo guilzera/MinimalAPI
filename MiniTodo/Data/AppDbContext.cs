0f14529e6ba0e0a2533f914f51894a2e701a3fe9
guilzera <guiguidefarias@hotmail.com>
2022-03-29 12:04:17 -0300

    Exemplo de Minial API.

---
+++ MiniTodo/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MiniTodo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=NOTEBOOK-5KP7FMEG\\SQLEXPRESS;Database=MiniApi;Trusted_Connection=True;");
+    }
+}
